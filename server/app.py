a7b4f60a886e83429085baeb0d8e3ed269b62eee
Diana <dsantamariat04@gmail.com>
2026-03-07 11:50:44 -0500

    funcionalidades administrador

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from routes.curso import curso_bp
 from routes.interaccion import interaccion_bp
 from routes.lti import lti_bp
 from routes.sesion import sesion_bp
+from routes.admin import admin_bp
 
 load_dotenv()
 
@@ -30,6 +31,7 @@ app.register_blueprint(curso_bp)
 app.register_blueprint(interaccion_bp)
 app.register_blueprint(lti_bp)
 app.register_blueprint(sesion_bp)
+app.register_blueprint(admin_bp)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 3000))
